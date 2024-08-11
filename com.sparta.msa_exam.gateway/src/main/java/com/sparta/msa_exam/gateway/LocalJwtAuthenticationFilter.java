05f9769170467de54799b6fac6833f43ca066777
gichani <thdrlcks97@gmail.com>
2024-08-12 07:46:54 +0900

    외부 요청 보호

---
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/LocalJwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.sparta.msa_exam.gateway;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class LocalJwtAuthenticationFilter implements GlobalFilter {
+
+    private static final Logger log = LoggerFactory.getLogger(LocalJwtAuthenticationFilter.class);
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if (path.equals("/auth/signIn")) {
+            return chain.filter(exchange);
+        }
+        String token = extractToken(exchange);
+
+        if (token == null || !validateToken(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private String extractToken(ServerWebExchange exchange) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+
+    private boolean validateToken(String token) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+            log.info("payload :: " + claimsJws.getPayload().toString());
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
